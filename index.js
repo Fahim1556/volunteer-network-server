093d3d721de13de7decbc77dda25081748096c61
Fahim1556 <49098556+Fahim1556@users.noreply.github.com>
2020-10-14 12:09:34 +0600

    second commit

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ require('dotenv').config()
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.mx72a.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true})
 client.connect(err => {
-  const taskCollection = client.db(process.env.DB_NAME).collection("services")
+  const taskCollection = client.db(process.env.DB_NAME).collection("volunteer-tasks")
    
   const userCollection = client.db(process.env.DB_NAME).collection("user-tasks")
 
