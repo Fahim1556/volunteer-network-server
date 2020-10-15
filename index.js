e3e40a7eef54598a316700b3c79aab461d493150
Fahim1556 <49098556+Fahim1556@users.noreply.github.com>
2020-10-16 04:34:22 +0600

    second commit

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const cors = require('cors')
 const MongoClient = require('mongodb').MongoClient
 const ObjectId = require('mongodb').ObjectId
 app.use(cors())
+
 app.use(bodyParser.json())
 require('dotenv').config()
 
@@ -14,7 +15,7 @@ require('dotenv').config()
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.mx72a.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true})
 client.connect(err => {
-  const taskCollection = client.db(process.env.DB_NAME).collection("volunteer-tasks")
+  const taskCollection = client.db(process.env.DB_NAME).collection("collection")
    
   const userCollection = client.db(process.env.DB_NAME).collection("user-tasks")
 
@@ -57,7 +58,7 @@ client.connect(err => {
 })
 
 
-app.get('/',(req,res)=>res.send("Welcome Developer Nazmul"))
+app.get('/',(req,res)=>res.send("Welcome our server site"))
 
 app.listen(process.env.PORT || port)
 
